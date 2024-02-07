f3268b8d23777f7cc1635346de59c554da757f9d
Gina Sciacchitano <gbell76767@gmail.com>
2024-02-06 19:55:01 -0500

    working logic but only some user input validation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# LogoGenerator
+# Logo Generator
+
+## Description
+Logo Generator is a command line application that creates simple svg files consisting of an up to 3 character logo and a basic shape.
+
+## Installation
+Uses VS code extension https://marketplace.visualstudio.com/items?itemName=jock.svg to be able to preview svg files
+
+## Usage
+Run index.js in the command line and answer the questions provided to supply the generator with a logo of up to 3 characters, text color, pick a shape from square triangle or circle, and shape color. When done, the logo will be generated in logo.svg which will be located in the examples folder. If logo.svg already exists when you run this program, it will be overwritten with your new logo. Right click on logo.svg and select 'Preview SVG' to see your logo.
+
+## Liscence
+N/A
---
+++ index.js
@@ -0,0 +1,99 @@
+const {Triangle, Square, Circle} = require('./lib/shapes')
+const inquierer = require('inquirer')
+const fs = require('fs')
+
+const getInput = async() => {
+
+    let logo = await inquierer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Which characters would you like your logo to use (up to 3 characters)?'
+        }
+    ])
+    while(logo.text.length > 3){
+        logo = await inquierer.prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Your logo contained too many characters. Please use up to 3 characters.'
+            }
+        ])
+    }
+
+    let textColor = await inquierer.prompt([
+        {
+            type: 'input',
+            name: 'color',
+            message: 'What color would you like your logo to be (please use valid color name or hex value ex. green or #00ff00)?'
+        }
+    ])
+    // no way to validate without DOM
+    // p.style.color = shapeColor.color
+    // while(!p.style.color){
+    //     shapeColor = await inquierer.prompt([
+    //         {
+    //             type: 'input',
+    //             name: 'color',
+    //             message: 'Your color could not be found. Please use valid color name or hex value (ex. green or #00ff00).'
+    //         }
+    //     ])
+    //     p.style.color = shapeColor.color
+    // }
+
+    const shape = await inquierer.prompt([
+        {
+            type: 'list',
+            message: 'Which shape would you like your logo to use?',
+            name: 'shapeChoice',
+            choices: ['Triangle', 'Circle', 'Square']
+        }
+    ])
+
+    let shapeColor = await inquierer.prompt([
+        {
+            type: 'input',
+            name: 'color',
+            message: 'What color would you like that shape to be (please use valid color name or hex value ex. green or #00ff00)?'
+        }
+    ])
+    // no way to validate without DOM
+    // p.style.color = shapeColor.color
+    // while(!p.style.color){
+    //     shapeColor = await inquierer.prompt([
+    //         {
+    //             type: 'input',
+    //             name: 'color',
+    //             message: 'Your color could not be found. Please use valid color name or hex value (ex. green or #00ff00).'
+    //         }
+    //     ])
+    //     p.style.color = shapeColor.color
+    // }
+    
+    let newShape
+    if(shape.shapeChoice === 'Triangle'){
+        newShape = new Triangle()
+    }else if(shape.shapeChoice === 'Square'){
+        newShape = new Square()
+    }else{
+        newShape = new Circle()
+    }
+    newShape.setColor(shapeColor.color)
+
+    const svg = `
+    <svg width="300" height="200">
+        ${newShape.render()}
+        <text x="150" y="100" font-size="60" text-anchor="middle" fill="${textColor.color}">${logo.text}</text>
+    </svg>`
+
+    fs.writeFile('./examples/logo.svg', svg, function(err){
+        if(err){
+            throw err
+        }else{
+            console.log('Generated logo.svg')
+        }
+    })
+
+}
+
+getInput()
